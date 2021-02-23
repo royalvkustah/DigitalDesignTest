b54e40e23fa0e283ba600b62239221d674e36a15
Boris <www-bory72@yandex.ru>
2021-02-23 12:52:28 +0300

    Добавьте файлы проекта.

---
+++ DigitalDesignTest/FileWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DigitalDesignTest
+{
+    public static class FileWriter
+    {
+
+        public static void WriteFile(Dictionary<string,int> dict)
+        {
+            using (StreamWriter sw=new StreamWriter(@"..\..\..\Files\words.txt", false))
+            {
+                foreach (var item in dict)
+                {
+                    sw.WriteLine(item.Key+"_________"+item.Value);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ DigitalDesignTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace DigitalDesignTest
+{
+    class Program
+    {
+
+        public static string pathPrefix = @"..\..\..\Files\";
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary = WordSplitter.Execute(pathPrefix+"book1.fb2");
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Key+"______"+item.Value);
+            }
+            FileWriter.WriteFile(dictionary);
+        }
+    }
+}
